90c2ea714429b96fa6ae28af497f70fe91382905
elkin5 <syselkin5@gmail.com>
2019-02-04 19:12:03 -0500

    Agregar materialize css
    npm i materialize-css --save
    npm i angular2-materialize --save
    npm i jquery@^2.2.4 --save
    npm i hammerjs --save
    "styles": [
                  "node_modules/materialize-css/dist/css/materialize.css",
                  "src/stiles.css"
                ],
                "scripts": [
                  "node_modules/jquery/dist/jquery.js",
                  "node_modules/hammerjs/hammer.js",
                  "node_modules/materialize-css/dist/js/materialize.js"
                ]
    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import que permite usar materialize-css
+import { MaterializeModule } from 'angular2-materialize';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
